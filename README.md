69d3d01dcfcc4169691ac9fa904998f0ee536532
Swayvil <swayvil@gmail.com>
2022-03-15 13:05:33 +0100

    Output paths by timestamp

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ go build
 ./check-duplicate-files <root directory path>
 ```
 
-Output:<br/>
+Output only for duplicates:<br/>
 ```
 <CRC32 Hash>;<Index>;<File path>
 ```
--- check-duplicate-files.go
+++ check-duplicate-files.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"hash/crc32"
 	"io/fs"
@@ -8,6 +9,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var crc32q *crc32.Table
@@ -32,7 +34,7 @@ func walk(s string, d fs.DirEntry, err error) error {
 	if !d.IsDir() {
 		fileBuffer, err := ioutil.ReadFile(s)
 		if err != nil {
-			log.Fatalf("Failed opening file: %s", err)
+			log.Fatalf("Failed to open file: %s", err)
 		}
 		checkSum := crc32.Checksum(fileBuffer, crc32q)
 		paths, exist := filesMap[checkSum]
@@ -49,8 +51,50 @@ func walk(s string, d fs.DirEntry, err error) error {
 
 func outputMap() {
 	for checkSum, paths := range filesMap {
-		for index, path := range paths {
-			fmt.Printf("%08x;%d;%s\n", checkSum, index, path)
+		if len(paths) > 1 { // Output only paths with duplicates
+			paths, err := bubbleSortPaths(paths)
+			if err != nil {
+				fmt.Printf("%s", err) // Non fatal error
+			} else {
+				for index, path := range paths {
+					fmt.Printf("%08x;%d;%s\n", checkSum, index+1, path)
+				}
+			}
 		}
 	}
+}
+
+func convertPathToTime(path string) (time.Time, error) {
+	layout := "2006-01-02_15.04.05"
+	filename := filepath.Base(path)
+
+	if len(filename) < 19 {
+		return time.Time{}, errors.New("Convertion failed, path too short " + filename + "\n")
+	}
+
+	t, err := time.Parse(layout, filename[0:19])
+	if err != nil {
+		return time.Time{}, err
+	}
+	return t, nil
+}
+
+func bubbleSortPaths(paths []string) ([]string, error) {
+	for i := 0; i < len(paths)-1; i++ {
+		for j := 0; j < len(paths)-i-1; j++ {
+			timeJ, errJ := convertPathToTime(paths[j])
+			if errJ != nil {
+				return nil, errJ
+			}
+			timeJ1, errJ1 := convertPathToTime(paths[j+1])
+			if errJ1 != nil {
+				return nil, errJ1
+			}
+			if timeJ.After(timeJ1) {
+				paths[j], paths[j+1] = paths[j+1], paths[j]
+			}
+		}
+	}
+
+	return paths, nil
 }
