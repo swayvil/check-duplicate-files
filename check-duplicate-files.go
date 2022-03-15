4f2be75a99e2c4655e9013dfc9d5724116a10758
Swayvil <swayvil@gmail.com>
2022-03-15 16:46:18 +0100

    Add move duplicate files

--- check-duplicate-files.go
+++ check-duplicate-files.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"hash/crc32"
@@ -9,22 +10,39 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
 var crc32q *crc32.Table
 var filesMap map[uint32][]string
+var isRemoveDuplicates bool
 
 func main() {
 	if len(os.Args) <= 1 {
-		log.Fatal("usage: check-duplicate-files <root directory path>")
+		log.Fatal("usage: check-duplicate-files <root directory path> [remove]")
 	} else {
 		srcDirPath := os.Args[1]
 		crc32q = crc32.MakeTable(0xD5828281)
 		filesMap = make(map[uint32][]string)
+		isRemoveDuplicates = false
+
+		if len(os.Args) > 2 && os.Args[2] == "remove" {
+			isRemoveDuplicates = true
+		}
+
 		filepath.WalkDir(srcDirPath, walk)
+		duplicates := outputMap()
+
+		if isRemoveDuplicates {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Printf("\n\nAre you sure you want to move %d duplicates to the directory \"REMOVED\" [Y/N] ", duplicates)
+			text, _ := reader.ReadString('\n')
+			if strings.Compare(text, "Y\n") == 0 {
+				moveDuplicates(srcDirPath)
+			}
+		}
 	}
-	outputMap()
 }
 
 func walk(s string, d fs.DirEntry, err error) error {
@@ -49,19 +67,26 @@ func walk(s string, d fs.DirEntry, err error) error {
 	return nil
 }
 
-func outputMap() {
+func outputMap() int {
+	duplicates := 0
+
 	for checkSum, paths := range filesMap {
 		if len(paths) > 1 { // Output only paths with duplicates
 			paths, err := bubbleSortPaths(paths)
 			if err != nil {
-				fmt.Printf("%s", err) // Non fatal error
+				log.Println(err) // Non fatal error
 			} else {
+				filesMap[checkSum] = paths // Update map, so paths are already sorted for remove function
 				for index, path := range paths {
 					fmt.Printf("%08x;%d;%s\n", checkSum, index+1, path)
+					if index > 0 {
+						duplicates++
+					}
 				}
 			}
 		}
 	}
+	return duplicates
 }
 
 func convertPathToTime(path string) (time.Time, error) {
@@ -69,7 +94,7 @@ func convertPathToTime(path string) (time.Time, error) {
 	filename := filepath.Base(path)
 
 	if len(filename) < 19 {
-		return time.Time{}, errors.New("Convertion failed, path too short " + filename + "\n")
+		return time.Time{}, errors.New("Convertion failed, path too short " + filename)
 	}
 
 	t, err := time.Parse(layout, filename[0:19])
@@ -95,6 +120,30 @@ func bubbleSortPaths(paths []string) ([]string, error) {
 			}
 		}
 	}
-
 	return paths, nil
+}
+
+func moveDuplicates(srcDirPath string) {
+	removedDirectory := filepath.Join(srcDirPath, "REMOVED")
+
+	if _, err := os.Stat(removedDirectory); errors.Is(err, os.ErrNotExist) { // Folder doesn't exist
+		err := os.Mkdir(removedDirectory, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for _, paths := range filesMap {
+		if len(paths) > 1 { // Move only paths with duplicates
+			for index, path := range paths {
+				if index > 0 {
+					filename := filepath.Base(path)
+					err := os.Rename(path, filepath.Join(removedDirectory, filename))
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
+		}
+	}
 }
